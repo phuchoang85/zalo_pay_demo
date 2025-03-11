a66d170e62847392728d1df34497151d0e23b8ea
phuchoang85 <114634047+phuchoang85@users.noreply.github.com>
2025-03-11 23:43:50 +0700

    first commit

---
+++ routes/payment.js
@@ -0,0 +1,128 @@
+const { default: axios } = require("axios");
+var express = require("express");
+const moment = require("moment");
+var router = express.Router();
+const CryptoJS = require('crypto-js');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+const config = {
+  app_id: "2553",
+  key1: "PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL",
+  key2: "kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz",
+  endpoint: "https://sb-openapi.zalopay.vn/v2/create",
+};
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { amount, urlCalbackSuccess, dataSave, description, nameUser } =
+      req.body;
+
+    if (!amount || !urlCalbackSuccess || !nameUser) {
+      return res.status(400).json({ error: "Thiếu dữ liệu đầu vào" });
+    }
+
+    const embed_data = {
+      redirecturl: urlCalbackSuccess,
+    };
+
+    const items = [
+      {
+        dataSave: dataSave, 
+        // dữ liệu bạn muốn lưu lại để khi thanh toán thành công sẽ xủ lý bên backend của mìnhmình
+      },
+    ];
+
+    const transID = Math.floor(Math.random() * 1000000);
+
+    const order = {
+      app_id: config.app_id, // id của config
+      app_trans_id: `${moment().format("YYMMDD")}_${transID}`, // mã đơn
+      app_user: nameUser, // tên người dùng
+      app_time: Date.now(), // ngày tạo
+      item: JSON.stringify(items), // dữ lưu muốn lưu
+      embed_data: JSON.stringify(embed_data), // sẽ dẫn qua đường dẫn này khi thanh toán thành công
+      amount: amount, // giá tiền
+      description: description, // mô tả
+      bank_code: "",
+      callback_url: "https://9f9b-1-54-207-235.ngrok-free.app//paments/callback",
+      // cái này khi tạo thanh toán thành công sẽ chuyển sang api để xử lý dữ liệu cho db của mình
+    };
+
+    // Tạo chữ ký bảo mật (MAC)
+    const data =
+      config.app_id +
+      "|" +
+      order.app_trans_id +
+      "|" +
+      order.app_user +
+      "|" +
+      order.amount +
+      "|" +
+      order.app_time +
+      "|" +
+      order.embed_data +
+      "|" +
+      order.item;
+    order.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
+
+    // Gửi request tạo đơn hàng đến ZaloPay
+    const orderResponse = await axios.post(config.endpoint, null, {
+      params: order,
+    });
+    if (orderResponse.status !== 200 || !orderResponse.data) {
+      console.error("Lỗi từ ZaloPay:", orderResponse.data);
+      return res.status(400).json({ data: orderResponse.data.message });
+    }
+    return res.status(200).json({ data: orderResponse.data });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ data: "Lỗi hệ thống" });
+  }
+});
+
+router.post("/callback", async (req, res) => {
+  let result = {};
+  try {
+    const { data: dataStr, mac: reqMac } = req.body;
+
+    if (!dataStr || !reqMac) {
+      return res.status(400).json({ error: "Dữ liệu callback không hợp lệ" });
+    }
+
+    const data = JSON.parse(JSON.parse(dataStr).item)[0];
+
+    let mac = CryptoJS.HmacSHA256(dataStr, config.key2).toString();
+    console.log("mac =", mac);
+
+    // kiểm tra callback hợp lệ (đến từ ZaloPay server)
+    if (reqMac !== mac) {
+      // callback không hợp lệ
+      result.return_code = -1;
+      result.return_message = "mac not equal";
+    } else {
+      // thanh toán thành công
+      // merchant cập nhật trạng thái cho đơn hàng
+      let dataJson = JSON.parse(dataStr, config.key2);
+      console.log(
+        "update order's status = success where app_trans_id =",
+        dataJson["app_trans_id"]
+      );
+
+      // TODO: Cập nhật trạng thái đơn hàng trong DB
+      // xử lý dữ liệu ở  đây lưu dữ liệu db vân vân
+
+      result.return_code = 1;
+      result.return_message = "success";
+    }
+  } catch (ex) {
+    result.return_code = 0; // ZaloPay server sẽ callback lại (tối đa 3 lần)
+    result.return_message = ex.message;
+  }
+
+  // thông báo kết quả cho ZaloPay server
+  res.json(result);
+});
+
+module.exports = router;
