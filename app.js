e6ea36d843aca058563aabdf5aebaf42ca4c7146
phuchoang85 <114634047+phuchoang85@users.noreply.github.com>
2025-03-13 20:35:39 +0700

    u

--- app.js
+++ app.js
@@ -1,55 +1,44 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var paymentRouter = require('./routes/payment');
+var indexRouter = require("./routes/index");
+var paymentRouter = require("./routes/payment");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
-var demoRouter = express.Router();
-
-demoRouter.use('/', indexRouter); 
-demoRouter.use('/payments', paymentRouter);  
-
-app.use('/demo', demoRouter);
+app.use("/", indexRouter);
+app.use("/payments", paymentRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-console.log("server is started")
+console.log("server is started");
 
 module.exports = app;
